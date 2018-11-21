244c60ecebce6a72a435fa09c7d15f2ddf948686
MatthieuWerbrouck <45234600+MatthieuWerbrouck@users.noreply.github.com>
2018-11-21 15:20:21 +0100

    maj 1

--- classes.py
+++
@@ -1,4 +0,0 @@
-from deroulementcreaperso import *
-
-creaPerso()
-
--- nom.py
+++
@@ -1,8 +0,0 @@
-def choisirNom()->str:
-	nom=""
-	test=""
-	while test=="":
-		nom=input("Veuillez saisir votre pseudo: ")
-		test=nom.strip()
-	nom= nom.capitalize()
-	return nom
